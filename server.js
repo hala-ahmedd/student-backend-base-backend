f14e4fc922307e6388dfb94039d0f049ad35dceb
Hala Ahmed <hm2300277@tkh.edu.eg>
2025-11-11 10:23:45 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ db.serialize(() => {
 
 // Start listening on the specified port
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log('Server is running on port ?');
 });
